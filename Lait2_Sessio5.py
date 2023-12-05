10607d08c4bedec56df514ef23541ea2093dd66e
Heikki000 <heikki.savolainen2@metropolia.fi>
2023-12-05 20:34:02 +0200

    valmis2

--- Lait2_Sessio5.py
+++ Lait2_Sessio5.py
@@ -15,19 +15,19 @@ def f(x):
 dx = 0.001
 x = 1
 
-derivaatta_2 = (f(x + dx) - f(x))/dx
-virhe_2 = np.abs(derivaatta_2 - np.cos(x))
-print('Derivaatta(2):', derivaatta_2, '     Virhe:', virhe_2)
+derivaatta_2_1 = (f(x + dx) - f(x))/dx
+virhe_2 = np.abs(derivaatta_2_1 - np.cos(x))
+print('Derivaatta(2):', derivaatta_2_1, '     Virhe:', virhe_2)
 
-derivaatta_3 = (f(x + dx) - f(x - dx)) / (2 * dx)
-virhe_3 = np.abs(derivaatta_3 - np.cos(x))
-print('Derivaatta(3):', derivaatta_3, '     Virhe:', virhe_3)
+derivaatta_2_2 = (f(x + dx) - f(x - dx)) / (2 * dx)
+virhe_3 = np.abs(derivaatta_2_2 - np.cos(x))
+print('Derivaatta(3):', derivaatta_2_2, '     Virhe:', virhe_3)
 
 #3. Laske funktion 𝑓(𝑥)=𝑒**(−𝑥**2) derivaatan numeerinen likiarvo
 # kolmipistekaavalla pisteessä x = 1.5. Käytä askelpituutta dx = 0.001.
 
 def f(x):
-    return np.e ** (-1*x **2)
+    return np.e ** (-1*x ** 2)
 x = 1.5
 derivaatta_3_1 = (f(x + dx) - f(x - dx)) / (2 * dx)
 print('Derivaatta(3_1):', derivaatta_3_1, '.')
