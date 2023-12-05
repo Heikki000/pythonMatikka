62e9dccc8043cce661e845e8b8a113a01846a376
Heikki000 <heikki.savolainen2@metropolia.fi>
2023-12-05 20:12:48 +0200

    3 commit

--- Lait2_Sessio5.py
+++ Lait2_Sessio5.py
@@ -1,13 +1,12 @@
 '''
 Tehtäviä
-3. Laske funktion 𝑓(𝑥)=𝑒**(−𝑥**2)
-derivaatan numeerinen likiarvo kolmipistekaavalla pisteessä x = 1.5. Käytä askelpituutta dx = 0.001.
+
 4. Laske funktion 𝑔(𝑥)=(𝑥−1) / (𝑥+1)
 derivaatan numeerinen likiarvo kolmipistekaavalla pisteessä x = 0.3. Käytä askelpituutta dx = 0.001.
 
 '''
 #1. Laske sympyllä esimerkin 2𝑥**2 − 3  derivaatta.
-from sympy import symbols, diff, exp, sin
+from sympy import symbols, diff
 x = symbols('x')
 derivative = diff(2 * x**2 - 3, x)
 
@@ -31,7 +30,15 @@ derivaatta_3 = (f(x + dx) - f(x - dx)) / (2 * dx)
 virhe_3 = np.abs(derivaatta_3 - np.cos(x))
 print('Derivaatta(3):', derivaatta_3, '     Virhe:', virhe_3)
 
-#
+#3. Laske funktion 𝑓(𝑥)=𝑒**(−𝑥**2) derivaatan numeerinen likiarvo
+# kolmipistekaavalla pisteessä x = 1.5. Käytä askelpituutta dx = 0.001.
+
+def f(y):
+    return np.e ** (-1*y **2)
+y = 1.5
+
+derivaatta_3_1 = (f(y + dx) - f(y - dx)) / (2 * dx)
+print('Derivaatta(3_1):', derivaatta_3_1,'.')
 
 
 
